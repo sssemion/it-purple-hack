340d6084a5655768e9aa218731bf6aa66bd8aff4
Fedor <fedor.avilov@yandex.ru>
2024-03-13 08:24:12 +0000

    add utils with generator

---
+++ utils/utils.py
@@ -0,0 +1,112 @@
+from nltk.tokenize import sent_tokenize
+
+
+class Chunker:
+    def __init__(self, max_chunk_len=2500, overlap_len=50):
+        self._max_chunk_len = max_chunk_len
+        self._overlap_len = overlap_len
+        self._tokenizer = sent_tokenize
+
+    @staticmethod
+    def _join_docs(docs, separator):
+        text = separator.join(docs).strip()
+        if text == "":
+            return None
+        else:
+            return text
+
+    def create_chunks(self, text, url, separator='\n'):
+        sep_len = len(separator)
+        tokenized_text = self._tokenizer(text, language='russian')
+        chunks = []
+        cur_chunk = []
+        total_len = 0
+
+        for sentence in tokenized_text:
+            sentence_len = len(sentence)
+            if total_len + sentence_len + (sep_len if len(cur_chunk) > 0 else 0) > self._max_chunk_len:
+                if total_len > self._max_chunk_len:
+                    pass
+                if len(cur_chunk) > 0:
+                    doc = self._join_docs(cur_chunk, separator)
+                    if doc is not None:
+                        chunks.append(doc)
+
+                    while (total_len > self._overlap_len or
+                           (total_len + sentence_len + (sep_len if len(cur_chunk) > 0 else 0) > self._max_chunk_len and
+                            total_len > 0)):
+                        total_len -= len(cur_chunk[0]) + (sep_len if len(cur_chunk) > 1 else 0)
+                        cur_chunk = cur_chunk[1:]
+            cur_chunk.append(sentence)
+            total_len += sentence_len + (sep_len if len(cur_chunk) > 1 else 0)
+
+        doc = self._join_docs(cur_chunk, separator)
+
+        if doc is not None:
+            chunks.append(doc)
+        urls = [url] * len(chunks)
+        return chunks, urls
+
+    def split_texts(self, texts):
+        chunks = []
+        urls = []
+        for ind, text in texts.iterrows():
+            cur_chunks, cur_urls = self.create_chunks(text.text, text.name)
+            chunks.extend(cur_chunks)
+            urls.extend(cur_urls)
+        return chunks, urls
+    
+    
+    
+
+class Generator:
+    def __init__(self, tokenizer, model, max_tokens=350):
+        self._model = model
+        self._tokenizer = tokenizer
+        self._max_tokens = max_tokens
+        self._system_prompt = """
+        Ты помощник по документам Банка России, твоя задача ответить на вопрос пользователя. Далее на вход тебе будут приходить вопросы пользователей
+        (формат: Вопрос пользователя <вопрос пользователя>) и документы (формат: Документ c названием <название документа> <содержание документа>).
+        Входные документы содержат в себе ответ на вопрос с большой вероятностью.
+        Просьбы и уточнения:
+        1. Очень внимательно отвечай на вопрос, ответ на него может быть очень большим, но скорее всего он содержится в данных тебе документах!
+        2. Ответ на вопрос может состоять из нескольких пунктов, поэтому, если ты нашел один пункт, то посмотри вперед, возможно их несколько!
+        3. Если ответ на заданный вопрос не содержится в документах, то НЕ ОТВЕЧАЙ. Пиши выражение "Нет такой информации"
+        Нужно ответить на вопрос пользователя.
+        Задание: посмотри на документы, и опираясь на них дай свой ответ на вопрос в конце.
+        \n"""
+
+    def _generate_text(self, prompt, temperature, num_beams, n=1):
+            encoded_input = self._tokenizer.encode_plus(prompt, return_tensors='pt')
+
+            encoded_input = {k: v.to(self._model.device) for k, v in encoded_input.items()}
+
+            resulted_tokens = self._model.generate(**encoded_input,
+                                                  max_new_tokens=self._max_tokens,
+                                                  do_sample=True,
+                                                  num_beams=num_beams,
+                                                  num_return_sequences=n,
+                                                  no_repeat_ngram_size=2,
+                                                  temperature=temperature,
+                                                  top_p=0.9,
+                                                  top_k=50)
+
+            resulted_texts = self._tokenizer.batch_decode(resulted_tokens, skip_special_tokens=True)
+
+            return resulted_texts
+
+    def get_answer(self, question, documents, urls, temperature=0.6, num_beams=4):
+        documents_retriever = ''
+        formatted_urls = 'Использованные документы:\n'
+        for index, url in enumerate(urls):
+            formatted_urls += f'{index+1}) {url} \n'
+        for i in range(len(documents)):
+            documents_retriever += f'Документ c номером {i}: {documents[i]} \n'
+
+        QA_PROMPT = self._system_prompt + "Сгенерируй ответ на вопрос по тексту. Текст: '{context}'. Вопрос: '{question}'."
+        answer = self._generate_text(QA_PROMPT.format(context=documents_retriever, question=question),
+                                     temperature=temperature, 
+                                     num_beams=num_beams)[-1]
+        answer = answer + '\n\n' + formatted_urls
+        return answer
+        
